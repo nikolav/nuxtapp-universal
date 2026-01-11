d79dc66d1e00b68807176b42d5c1f895b0a12e54
Vukovic Nikola <admin@nikolav.rs>
2026-01-11 23:50:45 +0100

    Sun, Jan 11, 2026 10:50:45 PM

---
+++ app/utils/core-has-own.ts
@@ -0,0 +1,5 @@
+const has_ = Function.prototype.call.bind(Object.prototype.hasOwnProperty);
+export const coreHasOwn = <TNode = unknown, TKey = any>(
+  node: TNode,
+  key: TKey
+) => <boolean>has_(Object(node), key);
