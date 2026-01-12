c893308c12b7e6c5deb3a7aeec8791b043620b0b
Vukovic Nikola <admin@nikolav.rs>
2026-01-12 06:20:39 +0100

    Mon, Jan 12, 2026  5:20:39 AM

--- app/plugins/emitters.ts
+++ app/plugins/emitters.ts
@@ -6,16 +6,4 @@ import type { IEventApp } from "~/types";
 export default defineNuxtPlugin((nuxtapp) => {
   const emitter$ = new Subject<IEventApp>();
   nuxtapp.vueApp.provide(TOKEN_appEmitter$, emitter$);
-
-  // stream NotificationPermission:granted
-  onNuxtReady(async () => {
-    // "default" | "denied" | "granted";
-    const p = await Notification.requestPermission();
-    if ("granted" === p) {
-      emitter$.next(<IEventApp<NotificationPermission>>{
-        type: useAppConfig().events.EVENT_notifications_granted,
-        payload: p,
-      });
-    }
-  });
 });
