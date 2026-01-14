167269f9bb5cdf453c23f40fbb6aadb9c5fd131a
Vukovic Nikola <admin@nikolav.rs>
2026-01-14 07:38:21 +0100

    Wed, Jan 14, 2026  6:38:21 AM

--- i18n/i18n.config.ts
+++ i18n/i18n.config.ts
@@ -19,4 +19,80 @@ export default defineI18nConfig(() => ({
   fallbackWarn: false,
   // modifiers: {},
   // pluralRules: {},
+  datetimeFormats: {
+    sr: {
+      short: {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+      },
+      long: {
+        weekday: "short",
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        hour12: false,
+      },
+      numeric: { year: "numeric", month: "2-digit", day: "2-digit" },
+    },
+    en: {
+      short: {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+      },
+      long: {
+        weekday: "short",
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        hour12: true,
+      },
+      numeric: { year: "numeric", month: "2-digit", day: "2-digit" },
+    },
+  },
+  numberFormats: {
+    en: {
+      currency: {
+        style: "currency",
+        currency: "USD",
+        currencyDisplay: "symbol",
+        notation: "standard",
+      },
+      decimal: {
+        style: "decimal",
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+        useGrouping: true,
+      },
+      percent: {
+        style: "percent",
+        useGrouping: false,
+        maximumFractionDigits: 0,
+      },
+    },
+    sr: {
+      currency: {
+        style: "currency",
+        currency: "RSD",
+        currencyDisplay: "symbol",
+        notation: "standard",
+      },
+      decimal: {
+        style: "decimal",
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+        useGrouping: true,
+      },
+      percent: {
+        style: "percent",
+        useGrouping: false,
+        maximumFractionDigits: 0,
+      },
+    },
+  },
 }));
