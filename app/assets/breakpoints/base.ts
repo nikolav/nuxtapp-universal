3a92526f2b384a53d9d9e488a8283f0db75b06ab
Vukovic Nikola <admin@nikolav.rs>
2026-01-14 12:25:37 +0100

    Wed, Jan 14, 2026 11:25:37 AM

--- app/assets/breakpoints/base.ts
+++ app/assets/breakpoints/base.ts
@@ -8,7 +8,8 @@ export const displayDefaults = <VuetifyOptions["display"]>{
     md: 959.98,
     lg: 1279.98,
     xl: 1919.98,
-    "2xl": 2559.98,
+    // "2xl": 2559.98,
+    xxl: 2559.98,
     huge: 3199.98,
   },
 };
