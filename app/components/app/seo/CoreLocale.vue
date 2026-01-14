7931b536fffb325757acc7f2853badf881bd788a
Vukovic Nikola <admin@nikolav.rs>
2026-01-14 06:27:28 +0100

    Wed, Jan 14, 2026  5:27:28 AM

--- app/components/app/seo/CoreLocale.vue
+++ app/components/app/seo/CoreLocale.vue
@@ -1,33 +1,27 @@
 <script setup lang="ts">
 const route = useRoute();
-const head = useLocaleHead();
+const i18nHead = useLocaleHead();
 const { t } = useI18n();
-const title = computed(() => t(String(route.meta.title ?? "TBD")));
-const description = computed(() => t(String(route.meta.description ?? "TBD")));
+
+const title = computed(() => t(String(route.meta.title ?? "#TBD")));
+const description = computed(() => t(String(route.meta.description ?? "#TBD")));
+
 useSeoMeta({
   title,
   description,
 });
 
+useHead(() => ({
+  htmlAttrs: {
+    lang: i18nHead.value.htmlAttrs!.lang,
+  },
+  link: [...(i18nHead.value.link || [])],
+  meta: [...(i18nHead.value.meta || [])],
+}));
+
 // @@eos
 </script>
 
 <template>
-  <Html :lang="head.htmlAttrs.lang" :dir="head.htmlAttrs.dir" />
-  <Link
-    v-for="link in head.link"
-    :key="link.key"
-    :id="link.key"
-    :rel="link.rel"
-    :href="link.href"
-    :hreflang="link.hreflang"
-  />
-  <Meta
-    v-for="meta in head.meta"
-    :key="meta.key"
-    :id="meta.key"
-    :property="meta.property"
-    :content="meta.content"
-  />
   <slot />
 </template>
