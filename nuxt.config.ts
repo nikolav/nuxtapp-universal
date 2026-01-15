e0de9824b0678d1c64730b4b59b275a7d7563d6a
Vukovic Nikola <admin@nikolav.rs>
2026-01-15 18:07:49 +0100

    Thu, Jan 15, 2026  5:07:49 PM

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,13 @@ const siteUrl = trimEnd(
 );
 const siteName = process.env.NUXT_SITE_NAME ?? "";
 
-const CWD = process.cwd();
+const apiBase = trimEnd(
+  PRODUCTION
+    ? process.env.NUXT_PUBLIC_API_BASE
+    : process.env.NUXT_PUBLIC_API_BASE_DEV,
+  "/"
+);
+
 export const defaultLocale = process.env.NUXT_PUBLIC_DEFAULT_LOCALE ?? "sr";
 
 export default defineNuxtConfig({
@@ -58,7 +64,7 @@ export default defineNuxtConfig({
       siteUrl,
       siteName,
       baseUrl: siteUrl,
-      apiBase: process.env.NUXT_PUBLIC_API_BASE,
+      apiBase,
       defaultLocale,
       gtmId: process.env.NUXT_PUBLIC_GTM_ID,
       analyticsEnabled: parseBoolean(process.env.NUXT_PUBLIC_ANALYTICS_ENABLED),
