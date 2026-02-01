a7d942466f57be305e40dc626a958ef7e8c4a71c
Vukovic Nikola <admin@nikolav.rs>
2026-02-01 04:38:25 +0100

    Sun, Feb  1, 2026  3:38:25 AM

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 // ## imports, external, internal
-import { memory as AuthMemoryService } from "~/services/auth";
 // ## config:const
 // ## nuxt:core
 // ## props / emits / v-model / v-model helper
@@ -32,50 +31,11 @@ definePageMeta({
 // ## provide / expose
 // ## io, events, websockets
 
-const token = ref<any>();
-const account = ref<any>(null);
-const auth = new AuthMemoryService();
-const ok = () => {
-  console.log({ auth });
-};
-const authRegister = async () => {
-  const tok = await auth.register({
-    email: "admin@nikolav.rs",
-    password: "admin@nikolav.rs",
-  });
-  token.value = tok;
-};
-
-const authLogin = async () => {
-  const tok = await auth.authenticate({
-    email: "admin@nikolav.rs",
-    password: "admin@nikolav.rs",
-  });
-
-  token.value = tok;
-};
-
-const getAccount = async () => {
-  account.value = await auth.account(token.value);
-};
-
 // @@eos
 </script>
 
 <template>
-  <section class="app-container-reset page--index">
-    <div class="flex justify-center p-2 gap-4">
-      <button @click="ok">ok</button>
-      <button @click="authLogin">auth:login</button>
-      <button @click="authRegister">auth:register</button>
-      <button @click="getAccount">auth:register</button>
-    </div>
-    <div>
-      <small>
-        <pre>{{ account }}</pre>
-      </small>
-    </div>
-  </section>
+  <section class="app-container-reset page--index"></section>
 </template>
 
 <!-- scoped component styles, default -->
