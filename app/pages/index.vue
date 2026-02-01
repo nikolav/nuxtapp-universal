fdedfa56725b9b9798b74c52bd951039c95c5f91
Vukovic Nikola <admin@nikolav.rs>
2026-02-01 13:22:44 +0100

    Sun, Feb  1, 2026 12:22:44 PM

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 // ## imports, external, internal
-import { useAuth } from "~/stores";
 // ## config:const
 // ## nuxt:core
 // ## props / emits / v-model / v-model helper
@@ -32,42 +31,11 @@ definePageMeta({
 // ## provide / expose
 // ## io, events, websockets
 
-const auth = useAuth();
-
 // @@eos
 </script>
 
 <template>
-  <section class="app-container-reset page--index">
-    <div class="flex justify-center gap-4">
-      <button
-        @click="
-          auth.authenticate({
-            email: 'admin@nikolav.rs',
-            password: 'admin@nikolav.rs',
-          })
-        "
-      >
-        auth:login
-      </button>
-      <button
-        @click="
-          auth.register({
-            email: 'admin@nikolav.rs',
-            password: 'admin@nikolav.rs',
-          })
-        "
-      >
-        auth:register
-      </button>
-      <button @click="auth.logout()">auth:logout</button>
-    </div>
-    <div>
-      <small>
-        <pre>{{ auth.account }}</pre>
-      </small>
-    </div>
-  </section>
+  <section class="app-container-reset page--index"></section>
 </template>
 
 <!-- scoped component styles, default -->
