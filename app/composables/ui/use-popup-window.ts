cbb47ff86010bbdeb82f86239baeee2b52c2e9fd
Vukovic Nikola <admin@nikolav.rs>
2026-02-01 21:07:28 +0100

    Sun, Feb  1, 2026  8:07:28 PM

--- app/composables/ui/use-popup-window.ts
+++ app/composables/ui/use-popup-window.ts
@@ -1,47 +1,46 @@
-import type { Subscription } from "rxjs";
+import { of } from "rxjs";
+import { switchMap } from "rxjs/operators";
+
 import type { TOrNoValue } from "~/types";
 
 export const usePopupWindow = () => {
   const { $window$ } = useNuxtApp();
 
-  let win_s: TOrNoValue<Subscription>;
   const target = ref<TOrNoValue<globalThis.WindowProxy>>(null);
 
-  const destroy = (force = false) => {
-    win_s?.unsubscribe();
+  const destroy = () => {
     target.value?.close();
-    if (force) {
-      setTimeout(() => {
-        target.value = null;
-      });
-    }
   };
 
-  const open = (url: string, name = "oauth", w = 420, h = 550) => {
-    destroy();
-    win_s = $window$.subscribe((window) => {
-      const document = window.document;
-      const dualScreenLeft = window.screenLeft ?? window.screenX;
-      const dualScreenTop = window.screenTop ?? window.screenY;
-      const width =
-        window.innerWidth ??
-        document.documentElement.clientWidth ??
-        window.screen.width;
-      const height =
-        window.innerHeight ??
-        document.documentElement.clientHeight ??
-        window.screen.height;
+  const open = (url: string, name = "oauth", w = 420, h = 550) =>
+    $window$.pipe(
+      switchMap((window) => {
+        const document = window.document;
+        const dualScreenLeft = window.screenLeft ?? window.screenX;
+        const dualScreenTop = window.screenTop ?? window.screenY;
+        const width =
+          window.innerWidth ??
+          document.documentElement.clientWidth ??
+          window.screen.width;
+        const height =
+          window.innerHeight ??
+          document.documentElement.clientHeight ??
+          window.screen.height;
 
-      const left = Math.max(0, (width - w) / 2 + dualScreenLeft);
-      const top = Math.max(0, (height - h) / 2 + dualScreenTop);
+        const left = Math.max(0, (width - w) / 2 + dualScreenLeft);
+        const top = Math.max(0, (height - h) / 2 + dualScreenTop);
 
-      target.value = window.open(
-        url,
-        name,
-        `scrollbars=yes,width=${w},height=${h},top=${top},left=${left}`,
-      );
-    });
-  };
+        destroy();
+        const wPopup = window.open(
+          url,
+          name,
+          `scrollbars=yes,width=${w},height=${h},top=${top},left=${left}`,
+        );
+
+        target.value = wPopup;
+        return of(wPopup);
+      }),
+    );
 
   return { target, open, destroy };
 };
