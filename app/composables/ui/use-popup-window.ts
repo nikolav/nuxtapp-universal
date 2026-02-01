470fcecf7429a50982a6b34ca9a13e11c94155d1
Vukovic Nikola <admin@nikolav.rs>
2026-02-01 21:08:53 +0100

    Sun, Feb  1, 2026  8:08:53 PM

--- app/composables/ui/use-popup-window.ts
+++ app/composables/ui/use-popup-window.ts
@@ -16,16 +16,20 @@ export const usePopupWindow = () => {
     $window$.pipe(
       switchMap((window) => {
         const document = window.document;
+        const screen = window.screen;
+
         const dualScreenLeft = window.screenLeft ?? window.screenX;
         const dualScreenTop = window.screenTop ?? window.screenY;
+
         const width =
           window.innerWidth ??
           document.documentElement.clientWidth ??
-          window.screen.width;
+          screen.width;
+
         const height =
           window.innerHeight ??
           document.documentElement.clientHeight ??
-          window.screen.height;
+          screen.height;
 
         const left = Math.max(0, (width - w) / 2 + dualScreenLeft);
         const top = Math.max(0, (height - h) / 2 + dualScreenTop);
