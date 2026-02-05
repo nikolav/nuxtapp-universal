e1b3d3ba270eb3bc8b2184d91b48c844660ecf25
Vukovic Nikola <admin@nikolav.rs>
2026-02-05 13:23:39 +0100

    Thu, Feb  5, 2026 12:23:39 PM

--- app/components/app/configuration/Theme.client.vue
+++ app/components/app/configuration/Theme.client.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
+import { useTheme } from "vuetify";
+
 const {
   theme: { darkRootClass },
 } = useAppConfig();
+const theme = useTheme();
+
+const themeIsDark = computed(() => theme.global.current.value.dark);
 
 // @@eos
 </script>
 
 <template>
-  <Html :class="{ [darkRootClass]: false }" />
+  <Html :class="{ [darkRootClass]: themeIsDark }" />
 </template>
 
 <!-- scoped component styles, default -->
