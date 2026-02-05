454ed573e173b576c0581b83b4dd709b1014e213
Vukovic Nikola <admin@nikolav.rs>
2026-02-05 13:14:06 +0100

    Thu, Feb  5, 2026 12:14:06 PM

---
+++ app/components/app/configuration/Theme.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const {
+  theme: { darkRootClass },
+} = useAppConfig();
+
+// @@eos
+</script>
+
+<template>
+  <Html :class="{ [darkRootClass]: false }" />
+</template>
+
+<!-- scoped component styles, default -->
+<style lang="scss" scoped></style>
+<!-- css modules, per-class hashing -->
+<style module></style>
+<!-- global styles, rare, prefer styles.scss -->
+<style lang="scss"></style>
