966d0ded35a4ee07cf8f63e59b5b3accdc600916
Vukovic Nikola <admin@nikolav.rs>
2026-02-13 07:36:46 +0100

    Fri, Feb 13, 2026  6:36:46 AM

--- app/types/index.ts
+++ app/types/index.ts
@@ -19,6 +19,7 @@ export type TOrNoValue<T = any> = T | undefined | null;
 export type TMaybeEmptySubject = TOrNoValue<Subject<void>>;
 export type TMaybePromise<T> = T | Promise<T>;
 export type TMaybeAsync<T> = T | Observable<T> | Promise<T>;
+export type TFnMaybeAsync<T = unknown> = () => TMaybeAsync<T>;
 export interface IEventApp<TEventAppPayload = unknown> {
   type: string;
   payload: TEventAppPayload;
