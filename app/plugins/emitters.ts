403e131dff395f5dd1a55f5bac8fbf7f24b94eb0
Vukovic Nikola <admin@nikolav.rs>
2026-02-15 21:19:40 +0100

    Sun, Feb 15, 2026  8:19:40 PM

--- app/plugins/emitters.ts
+++ app/plugins/emitters.ts
@@ -3,7 +3,10 @@ import { Subject } from "rxjs";
 import { TOKEN_appEmitter$ } from "~/keys";
 import type { IEventApp } from "~/types";
 
-export default defineNuxtPlugin((nuxtapp) => {
-  const emitter$ = new Subject<IEventApp>();
-  nuxtapp.vueApp.provide(TOKEN_appEmitter$, emitter$);
+export default defineNuxtPlugin({
+  name: "emitters",
+  setup: (nuxtapp) => {
+    const emitter$ = new Subject<IEventApp>();
+    nuxtapp.vueApp.provide(TOKEN_appEmitter$, emitter$);
+  },
 });
--- app/plugins/vuetify.ts
+++ app/plugins/vuetify.ts
@@ -2,6 +2,7 @@ import { createVuetify } from "vuetify";
 import { md3 } from "vuetify/blueprints";
 import { aliases, mdi } from "vuetify/iconsets/mdi-svg";
 import { srLatn, en, srCyrl } from "vuetify/locale";
+import { filter, map } from "rxjs/operators";
 
 import DayJsAdapter from "@date-io/dayjs";
 import enDayjs from "dayjs/locale/en";
@@ -11,133 +12,138 @@ import srCyrlDayjs from "dayjs/locale/sr-cyrl";
 // use aliases
 import { VBtn } from "vuetify/components/VBtn";
 
+import { TOKEN_appEmitter$ } from "~/keys";
 import { light, dark } from "~/assets/themes";
 import { displayDefaults as display } from "~/assets/breakpoints";
 import { DatetimeService } from "~/plugins/datetime";
 
-export default defineNuxtPlugin((nuxtApp) => {
-  const { defaultLocale } = useRuntimeConfig().public;
-  const i18n = nuxtApp.vueApp.$nuxt.$i18n;
-  const vuetify = createVuetify({
-    ssr: useRuntimeConfig().public.ssr,
-    blueprint: md3,
+export default defineNuxtPlugin({
+  name: "vuetify",
+  dependsOn: ["emitters"],
+  setup: (nuxtApp) => {
+    const { defaultLocale } = useRuntimeConfig().public;
+    const vuetify = createVuetify({
+      ssr: useRuntimeConfig().public.ssr,
+      blueprint: md3,
 
-    // @useDisplay composable configuration options
-    // https://next.vuetifyjs.com/en/features/display-and-platform/#interface
-    // sync with tailwindcss.config.screens
-    display,
+      // @useDisplay composable configuration options
+      // https://next.vuetifyjs.com/en/features/display-and-platform/#interface
+      // sync with tailwindcss.config.screens
+      display,
 
-    // https://vuetifyjs.com/en/features/theme/#custom-themes
-    // https://next.vuetifyjs.com/en/features/theme/#theme-object-structure
-    theme: {
-      defaultTheme: "system",
-      themes: {
-        light,
-        dark,
+      // https://vuetifyjs.com/en/features/theme/#custom-themes
+      // https://next.vuetifyjs.com/en/features/theme/#theme-object-structure
+      theme: {
+        defaultTheme: "system",
+        themes: {
+          light,
+          dark,
+        },
+        variations: {
+          colors: [
+            "primary",
+            "secondary",
+            "success",
+            "info",
+            "warning",
+            "error",
+            // "primary2",
+            // "primary3",
+            // "accent1",
+            // "accent2",
+            // "complement",
+          ],
+          lighten: 1,
+          darken: 1,
+        },
+        layers: true,
+        // cspNonce: "foo",
+        // stylesheetId: "122",
       },
-      variations: {
-        colors: [
-          "primary",
-          "secondary",
-          "success",
-          "info",
-          "warning",
-          "error",
-          // "primary2",
-          // "primary3",
-          // "accent1",
-          // "accent2",
-          // "complement",
-        ],
-        lighten: 1,
-        darken: 1,
-      },
-      layers: true,
-      // cspNonce: "foo",
-      // stylesheetId: "122",
-    },
 
-    aliases: {
-      AppVBtn: VBtn,
-    },
-
-    // component/alias props
-    defaults: {
-      global: {
-        // ripple: true,
-      },
-      AppVBtn: {
-        color: "transparent",
-        rounded: true,
-      },
-      // MyButton: {
-      //   color: 'primary',
-      //   variant: 'tonal',
-      // },
-      // VCard: {
-      //   MyButton: { color: 'secondary' },
-      //   VBtn: { color: 'primary' },
-      // },
-      VCol: {
-        cols: 12,
-      },
-      VTooltip: {
-        // openDelay: TOOLTIPS_OPEN_DELAY,
-        // location: "bottom",
-        // activator: "parent",
-      },
-      // VMenu: {
-      //   transition: DEFAULT_TRANSITION,
-      // },
-      VForm: {
-        autocomplete: "off",
-      },
-      // <CustomComponent>: {
-      //   "foo:1": "bar",
-      // },
-    },
-
-    icons: {
-      defaultSet: "mdi",
       aliases: {
-        ...aliases,
-        // # override
-        // menu: IconMenu,
-        // # add: <VIcon icon="$iconCustom">
-        // iconCustom: IconCustom,
+        AppVBtn: VBtn,
       },
-      sets: {
-        mdi,
+
+      // component/alias props
+      defaults: {
+        global: {
+          // ripple: true,
+        },
+        AppVBtn: {
+          color: "transparent",
+          rounded: true,
+        },
+        // MyButton: {
+        //   color: 'primary',
+        //   variant: 'tonal',
+        // },
+        // VCard: {
+        //   MyButton: { color: 'secondary' },
+        //   VBtn: { color: 'primary' },
+        // },
+        VCol: {
+          cols: 12,
+        },
+        VTooltip: {
+          // openDelay: TOOLTIPS_OPEN_DELAY,
+          // location: "bottom",
+          // activator: "parent",
+        },
+        // VMenu: {
+        //   transition: DEFAULT_TRANSITION,
+        // },
+        VForm: {
+          autocomplete: "off",
+        },
+        // <CustomComponent>: {
+        //   "foo:1": "bar",
+        // },
       },
-    },
 
-    locale: {
-      locale: i18n.locale.value ?? defaultLocale,
-      fallback: defaultLocale,
-      messages: { sr: srLatn, "sr-cyrl": srCyrl, en },
-    },
+      icons: {
+        defaultSet: "mdi",
+        aliases: {
+          ...aliases,
+          // # override
+          // menu: IconMenu,
+          // # add: <VIcon icon="$iconCustom">
+          // iconCustom: IconCustom,
+        },
+        sets: {
+          mdi,
+        },
+      },
 
-    date: {
-      adapter: DayJsAdapter,
       locale: {
-        sr: srDayjs,
-        "sr-cyrl": srCyrlDayjs,
-        en: enDayjs,
+        locale: nuxtApp.vueApp.$nuxt.$i18n.locale.value || defaultLocale,
+        fallback: defaultLocale,
+        messages: { sr: srLatn, "sr-cyrl": srCyrl, en },
       },
-      formats: DatetimeService.FORMAT,
-    },
-  });
 
-  nuxtApp.vueApp.use(vuetify);
+      date: {
+        adapter: DayJsAdapter,
+        locale: {
+          sr: srDayjs,
+          "sr-cyrl": srCyrlDayjs,
+          en: enDayjs,
+        },
+        formats: DatetimeService.FORMAT,
+      },
+    });
 
-  // sync vuetify locale with i18n
-  watch(
-    () => i18n.locale.value,
-    (loc) => {
-      vuetify.locale.current.value = loc ?? defaultLocale;
-    },
-    { immediate: true },
-  );
+    nuxtApp.vueApp.use(vuetify);
+
+    const emitter$ = inject(TOKEN_appEmitter$)!;
+    emitter$
+      .pipe(
+        filter((e) => e.type === useAppConfig().events.EVENT_LOCALE_CHANGE),
+        map((e) => <string>e.payload),
+      )
+      .subscribe((localeCurrent) => {
+        vuetify.locale.current.value = localeCurrent || defaultLocale;
+      });
+  },
 });
 
 declare module "vuetify" {
