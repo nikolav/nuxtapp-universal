0318a6f98dce2385d376cc20c4f05547fda53535
Vukovic Nikola <admin@nikolav.rs>
2026-02-16 06:04:42 +0100

    Mon, Feb 16, 2026  5:04:42 AM

--- app/composables/utils/use-pending.ts
+++ app/composables/utils/use-pending.ts
@@ -1,16 +1,16 @@
 import type { TWatchDep } from "~/types";
 
-type TUsePendingDeps = Record<string, TWatchDep>;
+type TUsePendingDeps<T = unknown> = Record<string, TWatchDep<T>>;
 
-export const usePending = () => {
+export const usePending = <T = unknown>() => {
   const { $$ } = useNuxtApp();
-  const deps = ref(<TUsePendingDeps>{});
+  const deps = ref(<TUsePendingDeps<T>>{});
 
   const pending = computed(() =>
     $$.some(deps.value, (dep) => Boolean(toValue(dep))),
   );
 
-  const track = (newDeps: TUsePendingDeps) => {
+  const track = (newDeps: TUsePendingDeps<T>) => {
     $$.copy(deps.value, newDeps);
   };
 
