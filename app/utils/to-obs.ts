bb75c51a6cafac0b52b8859278e481eb7ebac91d
Vukovic Nikola <admin@nikolav.rs>
2026-02-22 06:35:37 +0100

    Sun, Feb 22, 2026  5:35:37 AM

--- app/utils/to-obs.ts
+++ app/utils/to-obs.ts
@@ -1,8 +1,30 @@
-import { defer, from, isObservable, of } from "rxjs";
+import { isRef, isReactive, isReadonly } from "vue";
+import type { WatchOptions } from "vue";
+import { defer, from, isObservable, Observable, of } from "rxjs";
+import { from as vufrom } from "@vueuse/rxjs";
+import isFunction from "lodash/isFunction";
+
 import type { TMaybeAsync } from "~/types";
 
-const isPromiseLike = (x: any): x is Promise<unknown> =>
-  !!x && typeof x.then === "function";
+const isPromiseLike = <T = unknown>(x: any): x is PromiseLike<T> =>
+  isFunction(Object(x).then);
 
-export const to$ = <T = unknown>(v: TMaybeAsync<T>) =>
-  defer(() => (isObservable(v) ? v : isPromiseLike(v) ? from(v) : of(v)));
+const isVueWatchSourceLike = (v: unknown): v is (() => unknown) | object =>
+  isFunction(v) || isRef(v) || isReactive(v) || isReadonly(v);
+
+export const to$ = <T = unknown>(
+  input: TMaybeAsync<T>,
+  watchOptins: WatchOptions = { immediate: true },
+) =>
+  defer(() => {
+    // Observable
+    if (isObservable(input)) return input;
+    // Promise / PromiseLike
+    if (isPromiseLike<T>(input)) return from(input);
+    // Vue reactivity / getter -> Observable (VueUse)
+    if (isVueWatchSourceLike(input)) {
+      return <Observable<T>>vufrom(<any>input, watchOptins);
+    }
+    // value
+    return of(input);
+  });
