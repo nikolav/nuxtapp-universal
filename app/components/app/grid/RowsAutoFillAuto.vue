d74e8e53d6e7957bda3ebc122866173f1a98663a
Vukovic Nikola <admin@nikolav.rs>
2026-02-26 09:08:09 +0100

    Thu, Feb 26, 2026  8:08:09 AM

---
+++ app/components/app/grid/RowsAutoFillAuto.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const props = defineProps<{
+  propsBody?: any;
+  propsTop?: any;
+  propsBottom?: any;
+}>();
+
+// @@eos
+</script>
+
+<template>
+  <section
+    class="app-container-reset component--AppGridTwoRowsBottomAuto grid grid-cols-1 grid-rows-auto-fill-auto"
+  >
+    <header class="app-container-reset" v-bind="props.propsTop">
+      <slot name="top" />
+    </header>
+    <div class="app-container-reset" v-bind="props.propsBody">
+      <slot />
+    </div>
+    <footer class="app-container-reset" v-bind="props.propsBottom">
+      <slot name="bottom" />
+    </footer>
+  </section>
+</template>
