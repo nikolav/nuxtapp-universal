89b2e23c3ac02f315b53f1a9df92df69dae2a854
Vukovic Nikola <admin@nikolav.rs>
2026-04-01 23:17:45 +0200

    components AppOverlayScreen

---
+++ app/assets/transitions/index.ts
@@ -0,0 +1,5 @@
+import { createCssTransition } from "vuetify/util/transitions";
+
+export const appTransitionSlideYR = createCssTransition(
+  "app-transition-slide-y-r",
+);
---
+++ app/components/app/overlay/Screen.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+defineOptions({
+  inheritAttrs: false,
+});
+
+const props = withDefaults(defineProps<{ propsSheet?: any }>(), {
+  propsSheet: {},
+});
+// @@eos
+</script>
+
+<template>
+  <ClientOnly>
+    <VDialog
+      fullscreen
+      :scrim="false"
+      transition="app-transition-slide-y-r"
+      v-bind="$attrs"
+    >
+      <VSheet
+        tile
+        elevation="0"
+        class="app-container-reset"
+        v-bind="props.propsSheet"
+      >
+        <slot />
+      </VSheet>
+    </VDialog>
+  </ClientOnly>
+</template>
+
+<!-- scoped component styles -->
+<style lang="scss" scoped></style>
+<!-- css modules, per-class hashing -->
+<style module></style>
+<!-- global styles -->
+<style lang="scss"></style>
