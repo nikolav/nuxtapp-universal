7251038b30d2826cc433a4208058c0259e8492f8
Vukovic Nikola <admin@nikolav.rs>
2026-04-02 00:25:57 +0200

    Wed, Apr  1, 2026 10:25:57 PM

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { useToggleFlag } from "~/composables/utils/use-toggle-flag";
+
 definePageMeta({
   title: "pages.index.title",
   description: "pages.index.description",
@@ -12,12 +14,36 @@ definePageMeta({
   },
 });
 
+const toggleActive = useToggleFlag();
+
 // @@eos
 </script>
 
 <template>
   <section class="app-container-reset page--index">
     <h2>page:home</h2>
+    <AppVBtn @click="toggleActive"> ok </AppVBtn>
+    <AppOverlayScreen v-model="toggleActive.isActive.value">
+      <VListItem>
+        <template #append>
+          <VBtn
+            icon
+            color="on-surface"
+            rounded="full"
+            variant="plain"
+            @click="toggleActive.off"
+          >
+            <IconX icon="$close" />
+          </VBtn>
+        </template>
+      </VListItem>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas, sapiente
+        cum. Numquam quam nostrum, molestias quasi dicta ut saepe libero, autem
+        laborum corrupti facilis maiores doloremque! Accusantium veritatis aut
+        esse.
+      </p>
+    </AppOverlayScreen>
   </section>
 </template>
 
