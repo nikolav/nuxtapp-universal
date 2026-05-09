436e2f11505f8b7659966827139004eae63a2c98
Vukovic Nikola <admin@nikolav.rs>
2026-05-09 13:36:05 +0200

    Sat, May  9, 2026 11:36:05 AM

--- app/pages/demo/index.vue
+++ app/pages/demo/index.vue
@@ -1,10 +1,24 @@
 <script setup lang="ts">
+import { useGravatars } from "~/stores/use-gravatars.store";
+import { useOnceMounted } from "~/composables/utils/use-once-mounted-on";
+
+const g = useGravatars();
+
+useOnceMounted([], () => {
+  g.start();
+});
+
 // @@eos
 </script>
 
 <template>
   <section class="app-container-reset page--demo">
     <h2>page:demo</h2>
+    <VBtn @click="() => g.enable(true)">enable</VBtn>
+    <VBtn @click="() => g.enable(false)">disable</VBtn>
+    <VBtn @click="() => g.refresh()">refresh</VBtn>
+    <pre>src: {{ g.src }}</pre>
+    <img :src="g.src" v-if="g.enabled" />
   </section>
 </template>
 
