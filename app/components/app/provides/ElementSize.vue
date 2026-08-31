050fb05adc89152942ecb2789ee7591eaf89b38b
Vukovic Nikola <admin@nikolav.rs>
2026-08-31 20:09:48 +0200

    component app/provides/ElementSize.vue

---
+++ app/components/app/provides/ElementSize.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import type { TOrNoValue, TVueInstance } from "~/types";
+import type { default as TAppBoxBase } from "~/components/app/box/Base.vue";
+
+const target = useTemplateRef<TOrNoValue<InstanceType<typeof TAppBoxBase>>>(
+  "target-945b03c3-ef77-56ae-9208-04ce7c28ca20",
+);
+
+const el_ = useCurrentElement((<unknown>target) as TVueInstance);
+const s = useElementSize(el_);
+
+const exposed = { ...s };
+
+defineExpose(exposed);
+
+// @@eos
+</script>
+
+<template>
+  <AppBoxBase
+    class="component--AppProvidesElementSize"
+    ref="target-945b03c3-ef77-56ae-9208-04ce7c28ca20"
+  >
+    <slot v-bind="s" />
+  </AppBoxBase>
+</template>
