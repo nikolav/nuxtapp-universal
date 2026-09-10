cac936364fe474f1d39f0fc108fe4fdb292b5473
Vukovic Nikola <admin@nikolav.rs>
2026-09-10 09:22:16 +0200

    Thu, Sep 10, 2026  7:22:16 AM

---
+++ app/components/app/grid/TwoCells.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    stack?: boolean;
+    invert?: boolean;
+    colsClass?: string;
+
+    propsMedia?: any;
+    propsText?: any;
+  }>(),
+  {
+    stack: false,
+    colsClass: "grid-cols-2",
+  },
+);
+
+// @@eos
+</script>
+
+<template>
+  <AppBoxBase
+    class="component--AppGridTwoCells"
+    :class="[props.stack ? undefined : `grid ${props.colsClass}`]"
+  >
+    <AppBoxBase
+      :class="
+        props.stack ? undefined : props.invert ? 'order-last' : 'order-first'
+      "
+      v-bind="props.propsMedia"
+    >
+      <slot name="media" />
+    </AppBoxBase>
+    <AppBoxBase
+      :class="
+        props.stack ? undefined : props.invert ? 'order-first' : 'order-last'
+      "
+      v-bind="props.propsText"
+    >
+      <slot name="text" />
+    </AppBoxBase>
+  </AppBoxBase>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 definePageMeta({
-  layout: "default",
+  layout: "debug",
   i18n: {
     paths: {
       sr: "/dobrodosli",
