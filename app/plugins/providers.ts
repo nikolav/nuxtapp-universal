91d8854aa303bf4bfa4875cae822d758ca69ae6c
Vukovic Nikola <admin@nikolav.rs>
2026-09-10 09:53:55 +0200

    Thu, Sep 10, 2026  7:53:55 AM

--- app/plugins/providers.ts
+++ app/plugins/providers.ts
@@ -1,9 +1,15 @@
-import { TOKEN_foo } from "~/keys";
+import { BehaviorSubject } from "rxjs";
+
+import type { TRecordJson } from "~/types";
+import { TOKEN_appState$, TOKEN_foo } from "~/keys";
 
 export default defineNuxtPlugin({
   name: "providers",
   enforce: "pre",
   setup: (nuxtapp) => {
     nuxtapp.vueApp.provide(TOKEN_foo, "foo");
+
+    const state$ = new BehaviorSubject<TRecordJson>({});
+    nuxtapp.vueApp.provide(TOKEN_appState$, state$);
   },
 });
