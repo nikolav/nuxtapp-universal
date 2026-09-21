ba72f176da0399bc2bf993aa4e00e856dccaf51c
Vukovic Nikola <admin@nikolav.rs>
2026-09-21 09:58:57 +0200

    Mon, Sep 21, 2026  7:58:57 AM

--- server/services/yoga.ts
+++ server/services/yoga.ts
@@ -1,19 +1,10 @@
 import { createSchema, createYoga } from "graphql-yoga";
 import resolvers from "#server/graphql/resolvers/index";
+import { typeDefs } from "#server/graphql/schema-graphql";
 
 export const yoga = createYoga({
   schema: createSchema({
-    typeDefs: /* GraphQL */ `
-      scalar JsonData
-
-      type Query {
-        status: JsonData!
-      }
-
-      type Mutation {
-        demo: JsonData!
-      }
-    `,
+    typeDefs,
     resolvers,
   }),
 });
