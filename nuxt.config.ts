70aee9119b9fa457b60ab72aeb842f3d34751523
Vukovic Nikola <admin@nikolav.rs>
2026-09-23 00:28:19 +0200

    Tue, Sep 22, 2026 10:28:19 PM

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -333,6 +333,7 @@ export default defineNuxtConfig({
         headers: { "cache-control": "public, max-age=31536000, immutable" },
       },
       "/api/**": { cors: true, headers: { "Cache-Control": "no-cache" } },
+      "/graphql": { cors: true, headers: { "Cache-Control": "no-cache" } },
     },
 
     // Optional Nitro storage adapter (Redis)
