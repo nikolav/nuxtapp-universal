a1d4893d9fda5c9b7ddebf52cc18bbec601a5f7a
Vukovic Nikola <admin@nikolav.rs>
2026-09-25 22:40:08 +0200

    Fri, Sep 25, 2026  8:40:08 PM

--- app/assets/themes/colors.ts
+++
@@ -1,5 +0,0 @@
-export const COLOR_PRIMARY = "#1A73E8"; // Google blue
-export const COLOR_SECONDARY = "#5F6368";
-
-export const COLOR_PRIMARY_DARK = "#4C8DFF";
-export const COLOR_SECONDARY_DARK = "#8A93A1";
